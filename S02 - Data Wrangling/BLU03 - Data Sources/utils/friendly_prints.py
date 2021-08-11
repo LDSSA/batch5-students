ef8e3db2ad6e5958ab4ba5b8e33e7ec2bb511ee2
LDSA Machine User <machine@lisbondatascience.org>
2021-08-11 11:36:05 +0000

    API bug was fixed, pushing again

---
+++ S02 - Data Wrangling/BLU03 - Data Sources/utils/friendly_prints.py
@@ -0,0 +1,9 @@
+def friendly_print_string(s):
+    print(s[:500])
+
+
+def friendly_print_soup_children(soup_children):
+    soup_children[3] = soup_children[3][:500]
+    
+    return soup_children
+        
